3a8a7bc3ee011c20162feb4b8c2de6e4cddee9ee
Hilton Pintor <hiltonpintor@gmail.com>
2017-09-15 12:12:40 -0300

    Steps 1-4

---
+++ thinking-in-react/src/FilterableProductTable.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import ProductTable from './ProductTable';
+
+class FilterableProductTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {filterText: '', inStockOnly: false};
+  }
+
+  render() {
+    var PRODUCTS = [
+      {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+      {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+      {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+      {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+      {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+      {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+    ];
+
+    return (
+      <div style={{padding: '10px'}}>
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+
+        <ProductTable
+          products={PRODUCTS}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+      </div>
+    );
+  }
+}
+
+
+
+export default FilterableProductTable;
---
+++ thinking-in-react/src/SearchBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input
+          type='text'
+          placeholder='Search...'
+          value={this.props.filterText}
+        />
+
+        <p>
+          <input
+            type='checkbox'
+            checked={this.props.inStockOnly}
+          />
+          only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
